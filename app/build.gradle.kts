b2ae8efef55241a8122243efb3703a788f6ffda6
a18561866215-cyber <a18561866215@gmail.com>
2026-08-26 11:10:55 +0800

    Add Android app module

---
+++ app/build.gradle.kts
@@ -0,0 +1,16 @@
+plugins {
+    id("com.android.application")
+}
+
+android {
+    namespace = "com.lan.networkmonitor"
+    compileSdk = 36
+
+    defaultConfig {
+        applicationId = "com.lan.networkmonitor"
+        minSdk = 29
+        targetSdk = 36
+        versionCode = 1
+        versionName = "0.1.0"
+    }
+}
