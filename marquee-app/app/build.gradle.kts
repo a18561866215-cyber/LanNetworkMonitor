6b00353a21b31d2f9f0bf7d5a643fe1f893d78be
a18561866215-cyber <a18561866215@gmail.com>
2026-08-26 16:08:38 +0800

    Add marquee app module config

---
+++ marquee-app/app/build.gradle.kts
@@ -0,0 +1,19 @@
+plugins {
+    id("com.android.application")
+}
+
+android {
+    namespace = "com.lan.marqueeboard"
+    compileSdk = 36
+
+    defaultConfig {
+        applicationId = "com.lan.marqueeboard"
+        minSdk = 29
+        targetSdk = 36
+        versionCode = 1
+        versionName = "0.1.0"
+    }
+}
+
+dependencies {
+}
